50e503ef497a0b68e68e6c68e50864511034066b
Knoobie <Knoobie@gmx.de>
2015-03-20 15:37:25 +0100

    added getListAsString

--- src/main/java/de/knoobie/project/clannadutils/common/ListUtils.java
+++ src/main/java/de/knoobie/project/clannadutils/common/ListUtils.java
@@ -7,4 +7,11 @@ public class ListUtils {
     public static boolean isEmpty(Collection list) {
         return list == null || list.isEmpty();
     }
+
+    public static String getListAsString(Collection list) {
+        if (isEmpty(list)) {
+            return StringUtils.EMPTY;
+        }
+        return String.join(", ", list);
+    }
 }
