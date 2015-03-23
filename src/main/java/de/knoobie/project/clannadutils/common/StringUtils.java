2358e2543805ec0fa0f47c229def8894ec8f8175
Knoobie <Knoobie@gmx.de>
2015-03-23 21:52:14 +0100

    getInteger optimize

--- src/main/java/de/knoobie/project/clannadutils/common/StringUtils.java
+++ src/main/java/de/knoobie/project/clannadutils/common/StringUtils.java
@@ -2,47 +2,50 @@ package de.knoobie.project.clannadutils.common;
 
 public class StringUtils {
 
-  public static final String EMPTY = "";  
-    
-  public static String removeNPE(String s) {
-    return s == null ? EMPTY : s;
-  }
+    public static final String EMPTY = "";
 
-  public static String trim(String s) {
-    return removeNPE(s).trim();
-  }
-
-  public static boolean isEmpty(String val) {
-    return isEmpty(val, true);
-  }
-  
-  public static boolean isEmpty(Object obj, String value){
-    return obj == null ? true : isEmpty(value, true);
-  }
-
-  public static boolean isEmpty(String val, boolean checkNPE) {
-    return checkNPE ? trim(val).isEmpty() : val.isEmpty();
-  }
-
-  public static String removeLastCharacters(String s, int chars) {
-    if (s == null || chars <= 0) {
-      return EMPTY;
+    public static String removeNPE(String s) {
+        return s == null ? EMPTY : s;
     }
-    return s.length() > chars ? s.substring(0, s.length() - chars) : EMPTY;
-  }
 
-  public static int getInteger(String source, int defaultValue, boolean ignoreExceptions) {
+    public static String trim(String s) {
+        return removeNPE(s).trim();
+    }
 
-    if (source == null || source.isEmpty() || source.trim().isEmpty()) {
-      return defaultValue;
+    public static boolean isEmpty(String val) {
+        return isEmpty(val, true);
     }
-    try {
-      return Integer.parseInt(source);
-    } catch (Throwable t) {
-      if (ignoreExceptions) {
-        return defaultValue;
-      }
-      throw t;
+
+    public static boolean isEmpty(Object obj, String value) {
+        return obj == null ? true : isEmpty(value, true);
     }
-  }
+
+    public static boolean isEmpty(String val, boolean checkNPE) {
+        return checkNPE ? trim(val).isEmpty() : val.isEmpty();
+    }
+
+    public static String removeLastCharacters(String s, int chars) {
+        if (s == null || chars <= 0) {
+            return EMPTY;
+        }
+        return s.length() > chars ? s.substring(0, s.length() - chars) : EMPTY;
+    }
+
+    public static int getInt(String source, int defaultValue, boolean ignoreExceptions) {
+        return getInteger(source, defaultValue, ignoreExceptions);
+    }
+
+    public static Integer getInteger(String source, Integer defaultValue, boolean ignoreExceptions) {
+        if (isEmpty(source)) {
+            return defaultValue;
+        }
+        try {
+            return Integer.parseInt(source);
+        } catch (Throwable t) {
+            if (ignoreExceptions) {
+                return defaultValue;
+            }
+            throw t;
+        }
+    }
 }
