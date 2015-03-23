c9493cc495cfe57dab0de0d5eba0b3f15659c2b5
Knoobie <Knoobie@gmx.de>
2015-03-23 21:51:46 +0100

    Initial Release

---
+++ src/main/java/de/knoobie/project/clannadutils/common/DateUtilsJ8.java
@@ -0,0 +1,53 @@
+package de.knoobie.project.clannadutils.common;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DateUtilsJ8 {
+
+    public static final String DATE_FORMAT_YEAR_MINUS_MONTH_MINUS_DAY = "YYYY-MM-dd";
+
+    public static Date getSQLDate(String timeString, Date defaultValue, boolean ignoreExceptions) {
+        if (StringUtils.isEmpty(timeString)) {
+            return defaultValue;
+        }
+
+        try {
+            LocalDateTime time = LocalDateTime.parse(timeString);
+            return new Date(time.toLocalDate().toEpochDay());
+
+        } catch (Throwable e) {
+            if (ignoreExceptions) {
+                return defaultValue;
+            }
+            throw e;
+        }
+    }
+
+    public static Date getSQLDate(String dateString, String dateFormat, Date defaultValue, boolean ignoreExceptions) {
+        return getSQLDate(dateString, dateFormat, Locale.ENGLISH, defaultValue, ignoreExceptions);
+    }
+
+    public static Date getSQLDate(String dateString, String dateFormat, Locale locale, Date defaultValue, boolean ignoreExceptions) {
+        if (StringUtils.isEmpty(dateString)
+                || StringUtils.isEmpty(dateFormat)
+                || locale == null) {
+            return defaultValue;
+        }
+
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(dateFormat, locale);
+            LocalDate date = LocalDate.parse(dateString, formatter);
+            return new Date(date.toEpochDay());
+
+        } catch (Throwable e) {
+            if (ignoreExceptions) {
+                return defaultValue;
+            }
+            throw e;
+        }
+    }
+}
