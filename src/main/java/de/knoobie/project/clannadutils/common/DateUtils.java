1d9a004278f06eddc3f1fac04cb7f48ca207c2d0
Knoobie <Knoobie@gmx.de>
2015-03-24 20:28:03 +0100

    added DATE_FORMAT_YEAR_MINUS_MONTH_MINUS_DAY

--- src/main/java/de/knoobie/project/clannadutils/common/DateUtils.java
+++ src/main/java/de/knoobie/project/clannadutils/common/DateUtils.java
@@ -8,31 +8,32 @@ import java.util.Date;
 
 public class DateUtils {
 
-  public static final String TAG_MONTAG_JAHR_UM_STUNDE_MINUTE_SEKUNDE = "dd.MM.yy 'um' hh:mm:ss";
-    
-  public static String getNow(String simpleDateFormat) {
-    DateFormat dfmt = new SimpleDateFormat(simpleDateFormat);
-    return dfmt.format(getNow());
-  }
-  
-  public static String getDateWithFormat(Date date, String simpleDateFormat){
-    DateFormat dfmt = new SimpleDateFormat(simpleDateFormat);
-    return dfmt.format(date);    
-  }
+    public static final String TAG_MONTAG_JAHR_UM_STUNDE_MINUTE_SEKUNDE = "dd.MM.yy 'um' hh:mm:ss";
+    public static final String DATE_FORMAT_YEAR_MINUS_MONTH_MINUS_DAY = "YYYY-MM-dd";
 
-  public static Date getNow() {
-    Calendar cal = Calendar.getInstance();
-    return cal.getTime();
-  }
+    public static String getNow(String simpleDateFormat) {
+        DateFormat dfmt = new SimpleDateFormat(simpleDateFormat);
+        return dfmt.format(getNow());
+    }
 
-  public static Date getFutureDate(int minutes) {
-    Calendar cal = Calendar.getInstance();
-    cal.add(Calendar.MINUTE, minutes);
-    return cal.getTime();
-  }
-  
-  public static Timestamp getTimestampNow(){
-    return new Timestamp(getNow().getTime());
-  }
+    public static String getDateWithFormat(Date date, String simpleDateFormat) {
+        DateFormat dfmt = new SimpleDateFormat(simpleDateFormat);
+        return dfmt.format(date);
+    }
+
+    public static Date getNow() {
+        Calendar cal = Calendar.getInstance();
+        return cal.getTime();
+    }
+
+    public static Date getFutureDate(int minutes) {
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.MINUTE, minutes);
+        return cal.getTime();
+    }
+
+    public static Timestamp getTimestampNow() {
+        return new Timestamp(getNow().getTime());
+    }
 
 }
