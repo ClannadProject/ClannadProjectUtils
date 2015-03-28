cfc375b166f63253efb4b167ed864880439efec4
Knoobie <Knoobie@gmx.de>
2015-03-28 17:18:40 +0100

    added isEmpty

--- src/main/java/de/knoobie/project/clannadutils/common/ArrayUtils.java
+++ src/main/java/de/knoobie/project/clannadutils/common/ArrayUtils.java
@@ -7,34 +7,38 @@ import java.util.List;
 
 public class ArrayUtils {
 
-  public static ArrayList<?> getListFromArray(Object[] array) {
-    return new ArrayList(Arrays.asList(array));
-  }
+    public static ArrayList<?> getListFromArray(Object[] array) {
+        return new ArrayList(Arrays.asList(array));
+    }
 
-  /**
-   * @todo EXPERIMENTELL!!!
-   * @param array
-   * @param containingObj
-   * @return
-   */
-  public static boolean contains(Object[] array, Object containingObj) {
-    if (containingObj == null) {
-      return false;
+    /**
+     * @todo EXPERIMENTELL!!!
+     * @param array
+     * @param containingObj
+     * @return
+     */
+    public static boolean contains(Object[] array, Object containingObj) {
+        if (containingObj == null) {
+            return false;
+        }
+        return new ArrayList(Arrays.asList(array)).contains(containingObj);
     }
-    return new ArrayList(Arrays.asList(array)).contains(containingObj);
-  }
 
-  public static <T> T getFirstItemFromList(List<T> list) {
-    if (list == null || list.isEmpty()) {
-      return null;
+    public static <T> T getFirstItemFromList(List<T> list) {
+        if (list == null || list.isEmpty()) {
+            return null;
+        }
+        return list.get(0);
     }
-    return list.get(0);
-  }
-  
-  public static <T> T getFirstItemFromCollection(Collection<T> list) {
-    if (list == null || list.isEmpty()) {
-      return null;
+
+    public static <T> T getFirstItemFromCollection(Collection<T> list) {
+        if (list == null || list.isEmpty()) {
+            return null;
+        }
+        return list.iterator().next();
     }
-    return list.iterator().next();
-  }
+
+    public static boolean isEmpty(Object[] array) {
+        return array == null || array.length <= 0;
+    }
 }
